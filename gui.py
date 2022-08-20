295b7436b1ece885c0b14df607bb27bb5ab03bc3
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 21:57:59 +0100

    disappearing 'type ingredient here' text

--- gui.py
+++ gui.py
@@ -13,9 +13,18 @@ img = PhotoImage(file='logo.png')
 Label(root,image=img,bg='#FFCD29').place(x=50,y=50)
 
 #ingredient entry
+def on_enter(e):
+    user.delete(0, 'end')
+def on_leave(e):
+    name=user.get()
+    if name=='':
+        user.insert(0, 'Type Ingredient Here')
+
 user = Entry(width=32,fg="black", border=1,bg="white",font=('Arial',12,'bold',))
 user.place(x=38, y=300)
 user.insert(0,'Type Ingredient Here')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
 
 Button(width=41,pady=7,text='Lets Eat!',bg='#00B3F0',fg='white',border=0,cursor='hand2').place(x=38, y=400)
 
