b86f34fb3e7ff34b41d02bf8574647291b26fc6c
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 16:23:35 +0100

    working code

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import requests
 app_id = '6d480379'
 app_key = 'ae61e680a47e6506290af41a2a66d862'
 
-ingredient = input("Type an ingredient name? ")
-url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
+q = input("Type an ingredient name? ")
+url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(q, app_id, app_key)
 
 response = requests.get(url)
 
 data = response.json()
 
-print(data['hits'])
-
-
+print(data['count'])
