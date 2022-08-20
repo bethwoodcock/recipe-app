1f025c218305ae8bbd5fbaca8dec46b7b1c1c86f
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 21:46:52 +0100

    button add

--- gui.py
+++ gui.py
@@ -13,11 +13,11 @@ img = PhotoImage(file='logo.png')
 Label(root,image=img,bg='#FFCD29').place(x=50,y=50)
 
 #ingredient entry
-user = Entry(width=32,fg="black", border=2,bg="white",font=('Arial',12,'bold',))
+user = Entry(width=32,fg="black", border=1,bg="white",font=('Arial',12,'bold',))
 user.place(x=38, y=300)
 user.insert(0,'Type Ingredient Here')
 
-
+Button(width=41,pady=7,text='Lets Eat!',bg='#00B3F0',fg='white',border=0,cursor='hand2').place(x=38, y=400)
 
 #run
 root.mainloop()
