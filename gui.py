34764d9028c089f4ddf1d64ce8924ba3347f06a4
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 19:12:09 +0100

    import frame

--- gui.py
+++ gui.py
@@ -4,18 +4,20 @@ from tkinter import messagebox
 def hello(e):
     print("Hello")
 
+#window
 root = Tk()
 root.title('Recipe App')
 root.geometry('375x667')
 root.configure(bg="#FFCD29")
 root.resizable(False,False)
-# btn = Button(tk, text = "click me", width = 30, height = 5)
-# btn.bind("<Button-1>", hello)
-# btn.pack()
 
+#change4life image
 img = PhotoImage(file='logo.png')
 Label(root,image=img,bg='#FFCD29').place(x=50,y=50)
 
+#ingredient frame
+frame=Frame(root,width=300,height=35,bg="white")
+frame.place(x=38, y=300)
 
-
+#run
 root.mainloop()
