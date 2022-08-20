f97e8b104b889c4620587c796a79387dead5c2e6
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 22:16:38 +0100

    lets eat window

--- gui.py
+++ gui.py
@@ -8,6 +8,14 @@ root.geometry('375x667')
 root.configure(bg="#FFCD29")
 root.resizable(False,False)
 
+#Eat Click
+def Eat():
+    window=Toplevel(root)
+    window.title("Results")
+    window.geometry('375x667')
+    window.configure(bg="#FFCD29")
+    window.resizable(False, False)
+
 #change4life image
 img = PhotoImage(file='logo.png')
 Label(root,image=img,bg='#FFCD29').place(x=50,y=50)
@@ -26,7 +34,8 @@ user.insert(0,'Type Ingredient Here')
 user.bind('<FocusIn>', on_enter)
 user.bind('<FocusOut>', on_leave)
 
-Button(width=41,pady=7,text='Lets Eat!',bg='#00B3F0',fg='white',border=0,cursor='hand2').place(x=38, y=400)
+#Lets Eat Button
+eat=Button(width=41,pady=7,text='Lets Eat!',bg='#00B3F0',fg='white',command=Eat,border=0,cursor='hand2').place(x=38, y=400)
 
 #run
 root.mainloop()
