ea63862679cd86057f4fe1ccb321e3c5ccb1c7c1
Bethany <bethwoodcockseo@gmail.com>
2022-08-20 18:53:27 +0100

    import image

--- gui.py
+++ gui.py
@@ -7,7 +7,15 @@ def hello(e):
 root = Tk()
 root.title('Recipe App')
 root.geometry('375x667')
+root.configure(bg="#FFCD29")
+root.resizable(False,False)
 # btn = Button(tk, text = "click me", width = 30, height = 5)
 # btn.bind("<Button-1>", hello)
 # btn.pack()
+
+img = PhotoImage(file='logo.png')
+Label(root,image=img,bg='#FFCD29').place(x=50,y=50)
+
+
+
 root.mainloop()
