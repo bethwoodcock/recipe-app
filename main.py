ea7d739423e2ac48bee65d806304b2014d61745b
Bethany <bethwoodcockseo@gmail.com>
2022-08-22 20:18:17 +0100

    tryna put it together

--- main.py
+++ main.py
@@ -1,103 +1,72 @@
-from io import BytesIO
-from py_edamam import PyEdamam
-import requests
+import re
 import tkinter as tk
-import webbrowser
+from tkinter import Tk, StringVar, Entry, Button, Text
 
-WINDOW_TITLE = "Recipe App"
-RECIPE_IMAGE_WIDTH = 350
-RECIPE_IMAGE_HEIGHT = 300
+import requests
 
-
-class RecipeApp(object):
-
-    def __init__(self, recipe_app_id, recipe_app_key):
-
-        self.recipe_app_id = recipe_app_id
-        self.recipe_app_key = recipe_app_key
-        self.window = tk.Tk()
-
-        # Auto resize geometry
-        self.window.geometry("")
-        self.window.configure(bg="#9ddfd3")
-        self.window.title(WINDOW_TITLE)
-
-        self.search_label = tk.Label(self.window, text="Search Recipe", bg="#ea86b6")
-        self.search_label.grid(column=0, row=0, padx=5)
-
-        self.search_entry = tk.Entry(master=self.window, width=40)
-        self.search_entry.grid(column=1, row=0, padx=5, pady=10)
-
-        self.search_button = tk.Button(self.window, text="search", highlightbackground="#ea86b6",
-                                       command=self.__run_search_query)
-        self.search_button.grid(column=2, row=0, padx=5)
-
-    def __run_search_query(self):
-
-        query = self.search_entry.get()
-        recipe = self.__get_recipe(query)
-
-        if recipe:
-            recipe_image = recipe.image
-            recipe_url = recipe.url
-
-        else:
-            # Recipe not found
-            recipe_image = "https://www.mageworx.com/blog/wp-content/uploads/2012/06/Page-Not-Found-13.jpg"
-            recipe_url = ""
-
-        self.__show_image(recipe_image)
-        self.__get_ingredients(recipe)
-
-        def __open_link():
-            webbrowser.open(recipe_url)
-
-        self.recipe_button = tk.Button(self.window, text="recipe link", highlightbackground="#ea86b6",
-                                       command=__open_link)
-        self.recipe_button.grid(column=1, row=7, pady=10)
-
-    def __get_recipe(self, variable):
-        edamam_object = PyEdamam(recipes_appid=self.recipe_app_id, recipes_appkey=self.recipe_app_key)
-        query_result = edamam_object.search_recipe(variable)
-
-        # Get first recipe in list
-        for recipe in query_result:
-            return recipe
-
-    def __show_image(self, image_url):
-        response = requests.get(image_url)
-
-        img = Image.open(BytesIO(response.content))
-        img = img.resize((RECIPE_IMAGE_WIDTH, RECIPE_IMAGE_HEIGHT))
-        image = ImageTk.PhotoImage(img)
-
-        holder = tk.Label(self.window, image=image)
-        holder.photo = image
-        holder.grid(column=1, row=6, pady=10)
-
-    def __get_ingredients(self, recipe):
-        ingredients = tk.Text(master=self.window, height=15, width=50, bg="#ffdada")
-        ingredients.grid(column=1, row=4, pady=10)
-        ingredients.delete("1.0", tk.END)
-
-        if recipe == None:
-            ingredients.insert(tk.END, "No Recipe found for search criteria")
-            return
-
-        ingredients.insert(tk.END, "\n" + recipe.label + "\n")
-        for ingredient in recipe.ingredient_names:
-            ingredients.insert(tk.END, "\n- " + ingredient)
-
-    def run_app(self):
-        self.window.mainloop()
-        return
+APP_ID = "18002c98"
+API_KEY = "46c5a4350bb1b9adaa4e67d27f703c01"
+API_URL = f'https://api.edamam.com/search?/app_id=${APP_ID}&app_key=${API_KEY}'
 
 
-# Create App and run the app
-if __name__ == "__main__":
-    # API Keys
-    APP_ID = "18002c98"
-    APP_KEY = "46c5a4350bb1b9adaa4e67d27f703c01"
+def on_enter(e):
+    recipe_input.delete(0, 'end')
 
-    recipe_app = RecipeApp(APP_ID, APP_KEY)
-    recipe_app.run_app()
+
+def on_leave(e):
+    name = recipe_input.get()
+    if name == '':
+        recipe_input.insert(0, 'Enter Your Ingredient')
+
+
+def search_recipe(name: str):
+    json_response = requests.get(f"{API_URL}").json()
+    flavor_text = json_response["flavor_text_entries"][0]["flavor_text"]
+    flavor_text = display_recipe_labels(data, index)
+    return flavor_text
+
+def display_recipe_labels(data, index):
+    """
+    Displays all recipe labels from a result of request.
+    Returns the max index of list of recipes.
+    """
+    print()
+    for recipe in data:
+        index += 1
+        print(f"   {index})", recipe['recipe']['label'])
+    print()
+    return index
+
+
+def get_description():
+    flavor_text = search_recipe(recipe_input.get().lower())
+    display.delete(1.0, tk.END)
+    display.insert(tk.END, flavor_text)
+
+
+# window
+root = Tk()
+root.title('Recipe App')
+root.geometry('375x667')
+root.configure(bg="#FFCD29")
+root.resizable(False, False)
+
+recipe = StringVar()
+recipe_input = Entry(root, textvariable=recipe, width=32, fg="black", border=1, bg="white",
+                      font=('Arial', 12, 'bold',))
+recipe_input.place(x=38, y=300)
+recipe_input.insert(0, 'Choose your Recipe')
+recipe_input.bind('<FocusIn>', on_enter)
+recipe_input.bind('<FocusOut>', on_leave)
+
+# Search button
+search_button = Button(width=41, pady=7, text='Search Recipes', bg='#00B3F0', fg='white', border=0, cursor='hand2',
+                       command=get_description)
+search_button.place(x=38, y=400)
+
+# Info Frame
+display = Text(root)
+display.place(x=38, y=450, relwidth=0.78, relheight=0.3)
+
+# run
+root.mainloop()
